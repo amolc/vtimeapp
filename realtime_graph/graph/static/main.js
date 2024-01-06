fe3ca97e5a8dc4338d39be6111f666494ce4c9ed
amolc <amol.c@fountaintechies.com>
2024-01-06 16:59:53 +0800

    websocket is working in the project

--- realtime_graph/graph/static/main.js
+++ realtime_graph/graph/static/main.js
@@ -1,1 +1,34 @@
-console.log("sss")
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+var graphData = {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 12, 3, 5, 30, 3,29],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+            ],
+           
+            borderWidth: 1
+        }]
+    },
+    
+}
+
+const myChart = new Chart(ctx,graphData);
+
+var socket = new WebSocket('ws://localhost:8000/ws/graph/')
+socket.onmessage = function (e){
+    var djangoData = JSON.parse(e.data);
+    console.log(djangoData)
+    var newGraphData = graphData.data.datasets[0].data ;
+    newGraphData.shift();
+    newGraphData.push(djangoData.value);
+    graphData.data.datasets[0].data = newGraphData ;
+    myChart.update();
+    document.querySelector('#app').innerText = djangoData.value ;
+
+}
